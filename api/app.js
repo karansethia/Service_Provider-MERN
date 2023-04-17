28b2a0bf6f51b3beac5f4f2fff6a80642c99df90
Karan <karansethia24@gmail.com>
2023-04-18 00:41:27 +0530

    Adding database connection

--- api/app.js
+++ api/app.js
@@ -1,7 +1,24 @@
+//! Packages
 import express from "express"
+import mongoose, { mongo } from "mongoose";
+import * as dotenv from 'dotenv'
 
+//! App initialization
 const app = express()
 
+dotenv.config()
+mongoose.set('strictQuery',true)
+const connect = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log('connected to mongodb');
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
 app.listen(3100,()=>{
+    connect()
     console.log("backend running");
 });
